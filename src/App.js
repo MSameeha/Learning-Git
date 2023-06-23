cf708ac72019ff15edb1d22837de70388ec1ff5a
Sameeha <sameehamubeen81@gmail.com>
2023-06-23 16:35:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <>
+      <div className='body'>
+        <div className="App">
+          <h2>Git Tutorial</h2>
+        </div>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
